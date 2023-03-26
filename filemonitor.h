2ee7f6cf8cccc4c02fa8f9be94ce06c7468fe155
Nastya F <fedorina.nastya@mail.ru>
2023-03-26 23:12:31 +0700

    Создание filemonitor

---
+++ filemonitor.h
@@ -0,0 +1,14 @@
+#ifndef FILEMONITOR_H
+#define FILEMONITOR_H
+
+class FileMonitor
+{
+public:
+    FileMonitor();
+private:
+    //void AddFile(); // добавляем файл под наблюдение
+    //void DelFile(); // исключаем файл из наблюдения
+
+}
+
+#endif // FILEMONITOR_H
