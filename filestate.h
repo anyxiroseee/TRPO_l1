bdc016801ee4ae09dffbc778295d54637dccf6a0
Nastya F <fedorina.nastya@mail.ru>
2023-03-27 19:26:49 +0700

    filestate.h update1

--- filestate.h
+++ filestate.h
@@ -1,18 +1,23 @@
 #ifndef FILESTATE_H
 #define FILESTATE_H
 #include <QFileInfo>
-
+#include <QString>
 
 class FileState
 {
-public:
-bool isExist; // файл существует?
-qint64 size = 0;
+
 private:
+QString fileName; //имя
+bool isExist; // файл существует?
+qint64 size = 0; //размер
+
+public:
 FileState();
+FileState (const QString& fileName);
+QString getfileName() const; //получаем имя файла
+bool getisExist() const; //проверяем, существует файл или нет
+qint64 getsize() const; //получаем размер
 
 }
-
-
 
 #endif // FILESTATE_H
