98654511a340ed60ba07b1025f3ea023961342ec
Nastya F <fedorina.nastya@mail.ru>
2023-06-06 11:32:51 +0700

    Перенос вывода в main
    изменила слоты на функции в коннектах сигналов и слотов

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
 #include "filestate.h"
 #include "filemonitor.h"
-#include "checkstatesfiles.h"
 #include <QCoreApplication>
 #include <iostream>
-
+#include <QObject>
+using namespace std;
 
 int main (int argc, char *argv[])
 {
+
  QCoreApplication a(argc, argv);
  setlocale(LC_ALL, "Russian");
- CheckStatesFiles Print;
  FileMonitor monitor;
 
-        QObject::connect(&monitor, &FileMonitor::startMonitoring, &Print, &CheckStatesFiles::startMonitorState);
-        QObject::connect(&monitor, &FileMonitor::endofMonitoring, &Print, &CheckStatesFiles::endofMonitorState);
-        QObject::connect(&monitor, &FileMonitor::checkChanged, &Print, &CheckStatesFiles::changedState);
-        QObject::connect(&monitor, &FileMonitor::checkDel, &Print, &CheckStatesFiles::deletedState);
-        QObject::connect(&monitor, &FileMonitor::checkExistence, &Print, &CheckStatesFiles::existState);
+   void changedState(QString fileName, qint64 size);
+   void startMonitorState(QString fileName, qint64 size);
+   void deletedState(QString fileName);
+   void endofMonitorState(QString fileName);
+   void existState(QString fileName, qint64 size);
+
+   QObject::connect(&monitor, &FileMonitor::startMonitoring, startMonitorState);
+   QObject::connect(&monitor, &FileMonitor::endofMonitoring, endofMonitorState);
+   QObject::connect(&monitor, &FileMonitor::checkChanged, changedState);
+   QObject::connect(&monitor, &FileMonitor::checkDel, deletedState);
+   QObject::connect(&monitor, &FileMonitor::checkExistence, existState);
 
     //добавляем файлы для отслеживания
     monitor.AddFile("C:/Users/anyxi/Desktop/forlab1/f1.txt");
@@ -33,4 +39,25 @@ int main (int argc, char *argv[])
 
        return a.exec();
 
+}
+
+void startMonitorState(QString fileName, qint64 Size) {
+        wcout << L"Файл добавлен под наблюдение: " << fileName.toStdWString() << L", размер: "<< Size << L" байт" << endl;
+}
+
+void endofMonitorState(QString fileName) {
+        wcout << L"Файл исключен из наблюдения: " << fileName.toStdWString() << endl;
+}
+
+void deletedState(QString fileName) {
+        wcout << L"Файл удален: " << fileName.toStdWString() << endl;
+}
+
+void changedState(QString fileName, qint64 Size) {
+        wcout << L"Файл изменен: " << fileName.toStdWString() << L", размер: "<< Size << L" байт"<< endl;
+}
+
+void existState(QString fileName, qint64 Size) {
+
+        wcout << L"Файл создан: "<<  fileName.toStdWString() << L", размер: " << Size << L" байт" << endl;
 }
